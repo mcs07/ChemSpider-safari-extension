36a15b47d8efb21e0eb5280605a123c4c4e19428
Matt Swain <m.swain@me.com>
2014-01-28 13:05:38 +0000

    Fix result parsing, redo dialog style
    - Fix issue where result wouldn't display if only a single result.
    - Improve parsing of ChemSpider search result HTML.
    - Remove Jquery UI and add more lightweight custom dialog code.
    - Add previous and next buttons to allow viewing of multiple results.
    - Restyle dialog to be all hip and flat ;-)
    - Stop using underscores in CSS classes and ids.

--- chemspider.safariextension/js/global.js
+++ chemspider.safariextension/js/global.js
@@ -1,44 +1,53 @@
 function performCommand(e) {
-	if (e.command !== 'chemspider')
-		return;
-	var query = e.userInfo;
-	query = query.replace(/\s+/g,'+');
-	var rUrl = 'http://www.chemspider.com/Search.aspx?q='+query;
-	$.get(rUrl, function(data) {
-		app.activeBrowserWindow.activeTab.page.dispatchMessage('searchResults', parseResults(data));
-	});
+    if (e.command !== 'chemspider') return;
+    // Generate id and tell injected script to show a loading dialog with that id
+    var id = 'chemspider-safari-extension-' + Date.now();
+    app.activeBrowserWindow.activeTab.page.dispatchMessage('showLoading', id);
+    // Make request to ChemSpider, then send results to injected script, with same id
+    var rUrl = 'http://www.chemspider.com/Search.aspx?q='+e.userInfo.replace(/\s+/g,'+');
+    $.get(rUrl, function(data, textStatus, jqXHR) {
+        var pUrl = jqXHR.getResponseHeader('x-orig-path'),
+            msg = {'id': id, 'results': parseResult(data, pUrl)};
+        app.activeBrowserWindow.activeTab.page.dispatchMessage('searchResults', msg);
+    });
 }
 
-function parseResults(data) {
-	var img = $(data).find('img[src*="ImagesHandler"]').get(0);
-	if (img) {
-		querystring = $(img).attr('src').split('?')[1];
-		var params = querystring.split('&');
-		for (var j=0;j<params.length;j++) {
-			var pair = params[j].split('=');
-			if (pair[0] == 'id') {
-				return pair[1];
-			} 
-		}
-	}
+function parseResult(data, url) {
+    // Parse the ChemSpider search result HTML for compound details
+    var page = $(data);
+    if (url == '/Search.aspx') {
+        return page.find('.search-id-column').map(function() {
+            return {
+                name: $(this).next().next().text(),
+                cid: $(this).find('a[href^="/Chemical-Structure."]').eq(0).text()
+            }
+        }).get();
+    } else if (url.indexOf('/Chemical-Structure.') !== -1) {
+        return [{
+            cid: cidpattern.exec(url)[1],
+            name: page.find('h1').text(),
+            //mf: page.find('.emp-formula').eq(0).html()
+        }];
+    }
 }
 
 function validateCommand(e) {
-	var selection = e.userInfo;
-	if (e.command !== 'chemspider' || selection===undefined) {
-		return;
-	}
-	if (selection.length == 0 || !selection) {
-		e.target.disabled = true;
-	}
-	if (selection.length > 25) {
-		selection = selection.substr(0,25);
-		selection = selection.replace(/^\s+|\s+$/g,'');
-		selection = selection + '...'
-	}
-	e.target.title = 'Search for "'+selection+'" on ChemSpider'; 
+    var selection = e.userInfo;
+    if (e.command !== 'chemspider' || selection === undefined) {
+        return;
+    }
+    // Only show ChemSpider context menu item if there is some selected text
+    if (selection.length == 0 || !selection) {
+        e.target.disabled = true;
+    }
+    // Truncate the menu item text if the selection is over 25 characters
+    if (selection.length > 25) {
+        selection = selection.substr(0, 25).replace(/^\s+|\s+$/g,'') + '...';
+    }
+    e.target.title = 'Search for "'+selection+'" on ChemSpider'; 
 }
 
 const app = safari.application;
+var cidpattern = /\/Chemical-Structure\.(\d+)\.html/
 app.addEventListener('command', performCommand, false);
 app.addEventListener('validate', validateCommand, false);
--- chemspider.safariextension/js/injected.js
+++ chemspider.safariextension/js/injected.js
@@ -1,63 +1,109 @@
-var locX, locY;
+var locX, locY, z_idx = 0;
 
-function contextMessage(e) {
-	locX = e.clientX + 10;
-    locY = e.clientY + 10;
-	var sel = '';
-	sel = window.parent.getSelection()+'';
-	sel = sel.replace(/^\s+|\s+$/g,'');
-	safari.self.tab.setContextMenuEventUserInfo(e, sel);
+function handleContextMenu(e) {
+    // Get mouse position, but limit to page size constraints
+    locX = Math.min(e.pageX + 10, $(document).width() - 200);
+    locY = Math.min(e.pageY + 10, $(document).height() - 300);
+    // Get selection and set as userInfo for global page
+    var sel = '';
+    sel = window.parent.getSelection()+'';
+    sel = sel.replace(/^\s+|\s+$/g,'');
+    safari.self.tab.setContextMenuEventUserInfo(e, sel);
 }
 
 function handleMessage(msg) {
-    if (msg.name === 'searchResults') {
-    	openWindow(msg.message);
+    if (window !== window.top) 
+        return
+    if (msg.name === 'showLoading') {
+        showLoading(msg.message);
+    } else if (msg.name === 'searchResults') {
+        displayResult(msg.message);
     }
 }
 
-function openWindow(id) {
-	if (window !== window.top) 
-		return;
-	if ($('link[href^="safari-extension://com.matt-swain.chemspider"][href$="css/chemspider.css"]').length == 0) {
-		$('<link rel="stylesheet" href="'+safari.extension.baseURI+'css/chemspider.css">').appendTo('head');
+function showLoading(id) {
+    if ($('link[href^="safari-extension://com.matt-swain.chemspider"][href$="css/chemspider.css"]').length == 0) {
+        $('<link rel="stylesheet" href="'+safari.extension.baseURI+'css/chemspider.css">').appendTo('head');
 	}
-	var $popupDiv = $('<div/>').appendTo('body');
-	if (!id) {
-		$('<div>No Results Found</div>').appendTo($popupDiv);
-		$popupDiv.dialog({
-			position: [locX,locY],
-			width: 154,
-			height: 190,
-			title: 'ChemSpider',
-			buttons: [
-				{text: 'Close', 'class':'__chemspider-dialog-button', click: function() {
-					$(this).dialog('close'); 
-				}},
-			]
-		});
-	} else {
-		var $img = $('<img src="http://www.chemspider.com/ImagesHandler.ashx?id='+id+'">').appendTo($popupDiv);
-		$popupDiv.dialog({
-			position: [locX,locY],
-			width: 154,
-			title: 'ChemSpider',
-			buttons: [
-				{text: '2D', 'class':'__chemspider-dialog-button', click: function() {
-					$popupDiv.html('<img src="http://www.chemspider.com/ImagesHandler.ashx?id='+id+'">');
-				}},
-				{text: '3D', 'class':'__chemspider-dialog-button', click: function() {
-					$popupDiv.html('<applet name="jmol" code="JmolApplet" archive="JmolApplet0.jar" codebase="http://www.chemspider.com/jmol" width="150" height="150" mayscript="true"><param name="progressbar" value="true"><param name="script" value="load http://www.chemspider.com/FilesHandler.ashx?type=str&amp;3d=yes&amp;id=24770180; set displayCellParameters FALSE; set antialiasDisplay ON;"></applet>');
-				}},
-				{text: 'Save', 'class':'__chemspider-dialog-button', click: function() {
-					location.href = 'http://www.chemspider.com/FilesHandler.ashx?type=str&id='+id;
-				}},
-				{text: 'More', 'class':'__chemspider-dialog-button', click: function() {
-					location.href = 'http://www.chemspider.com/Chemical-Structure.'+id+'.html';
-				}}
-			]
-		});
-	}
+    var dialog = $('<div class="chemspider-safari-extension" />').attr('id', id),
+        title = $('<div class="cssse-title">ChemSpider</div>')
+        spinner = $('<div class="cssse-spinner" />'),
+        close = $('<button type="button" class="cssse-close">×</button>')
+    close.click(function() { $(this).parent().fadeOut(); });
+    title.css({backgroundImage: 'url('+safari.extension.baseURI+'img/icon-small.png)'});
+    dialog.append(close).append(title).append(spinner);
+    dialog.css({top: locY, left: locX, display: 'none'});
+    dialog.drags({handle: '.cssse-title'})
+    dialog.appendTo('body').fadeIn();
 }
 
-document.addEventListener('contextmenu', contextMessage, false);
+function displayResult(msg) {
+    var dialog = $('#'+msg.id);
+    $('div.cssse-spinner', dialog).fadeOut(function() {
+        if (msg.results.length < 1) {
+            $('<div class="cssse-noresults">No Results Found</div>').appendTo(dialog);
+        } else {
+            if (msg.results.length > 1) {
+		        $('<button type="button" class="cssse-prev cssse-arrow">⟨</button>').click(function() {
+		            $('.cssse-slider', $(this).parent()).scrollTo({top:0, left:'-=175'}, 400);
+		        }).appendTo(dialog);
+		    }
+            //$('.title', dialog).eq(0).html('CID <span class="cid">'+msg.results[0].cid+'</span>');
+            var slider = $('<div class="cssse-slider" />');
+            $.each(msg.results, function(i, result) {
+                var img = $('<img src="http://www.chemspider.com/ImagesHandler.ashx?id='+result.cid+'&w=175&h=175" title="CID'+result.cid+' - '+result.name+'">'),
+                    save = $('<button type="button" title="Save MOL file for CID'+result.cid+'" class="cssse-save">Save</button>'),
+                    view = $('<button type="button" title="View CID'+result.cid+' on ChemSpider" class="cssse-view">View</button>'),
+                    cap = $('<div class="cssse-caption">CID <span class="cssse-cid">'+result.cid+'</span></div>');
+                save.click(function() {
+                    location.href = 'http://www.chemspider.com/FilesHandler.ashx?type=str&id='+result.cid;
+                });
+                view.click(function() {
+                    location.href = 'http://www.chemspider.com/Chemical-Structure.'+result.cid+'.html';
+                }); 
+			    $('<div class="cssse-mol" />').append(img).append(save).append(view).append(cap).appendTo(slider);
+		    });
+		    slider.hide().appendTo(dialog);
+		    dialog.animate({height: slider.height()+36}, 500, function () {
+		        slider.fadeIn(500);
+            });
+		    if (msg.results.length > 1) {
+		        $('<button type="button" class="cssse-next cssse-arrow">⟩</button>').click(function() {
+		            $('.cssse-slider', $(this).parent()).scrollTo({top:0, left:'+=175'}, 400);
+		        }).appendTo(dialog);
+		    }
+		    
+        }
+    });
+}
+
+// Draggable code
+(function($) {
+    $.fn.drags = function(opt) {
+        opt = $.extend({cursor:'move'}, opt);
+        var $el = this.find(opt.handle);
+        return $el.css('cursor', opt.cursor).on('mousedown', function(e) {
+            var $drag = $(this).addClass('cssse-active-handle').parent().addClass('cssse-draggable');
+            z_idx = Math.max(5001, z_idx+1);
+            var drg_h = $drag.outerHeight(),
+                drg_w = $drag.outerWidth(),
+                pos_y = $drag.offset().top + drg_h - e.pageY,
+                pos_x = $drag.offset().left + drg_w - e.pageX;
+            $drag.css('z-index', z_idx).parents().on('mousemove', function(e) {
+                $('.cssse-draggable').offset({
+                    top:e.pageY + pos_y - drg_h,
+                    left:e.pageX + pos_x - drg_w
+                }).on('mouseup', function() {
+                    $(this).removeClass('cssse-draggable');
+                });
+            });
+            e.preventDefault(); // disable selection
+        }).on('mouseup', function() {
+            $(this).removeClass('cssse-active-handle').parent().removeClass('cssse-draggable');
+        });
+    }
+})(jQuery);
+
+
+document.addEventListener('contextmenu', handleContextMenu, false);
 safari.self.addEventListener('message', handleMessage, false);
